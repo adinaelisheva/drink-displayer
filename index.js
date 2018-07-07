9f2341e6512952753ae52c2a3c7ee2999b9ea702
Adina <adinarubinoff@gmail.com>
2018-07-07 15:51:51 -0400

    math is in

--- index.js
+++ index.js
@@ -1,125 +1,3 @@
-const drinkMap = {
-  'bourbon': {
-    'color': '#A18950',
-    'pattern': 'stripes',
-    'patternColor': '#000000',
-  },
-  'whiskey': {
-    'color': '#CCB47C',
-    'pattern': 'stripes',
-    'patternColor': '#000000',
-  },
-  'brandy': {
-    'color': '#968432',
-    'pattern': 'stripes',
-    'patternColor': '#CCC08D',
-  },
-  'rum': {
-    'color': '#D6C163',
-  },
-  'creme de menthe': {
-    'color': '#DFF7D5',
-  },
-  'creme de cacao': {
-    'color': '#D6C47A',
-  },
-  'kahlua': {
-    'color': '#AD7F42',
-  },
-  'tequila': {
-    'color': '#E6FAE6',
-  },
-  'gin': {
-    'color': '#F7F5B5',
-  },
-  'vodka': {
-    'color': '#DEF9FF',
-  },
-  'cola': {
-    'color': '#B5A276',
-    'pattern': 'dots',
-    'patternColor': '#E0D8C5',
-  },
-  'sprite': {
-    'color': '#F7FAAF',
-    'pattern': 'dots',
-    'patternColor': '#ffffff',
-  },
-  'champagne': {
-    'color': '#F2E896',
-    'pattern': 'dots',
-    'patternColor': '#ffffff',
-  },
-  'iced tea': {
-    'color': '#E3BA3D',
-  },
-  'cherry juice': {
-    'color': '#9C4343',
-  },
-  'ginger ale': {
-    'color': '#DEC357',
-    'pattern': 'dots',
-    'patternColor': '#ffffff',
-  },
-  'lime juice': {
-    'color': '#9FF27E',
-  },
-  'lemon juice': {
-    'color': '#F5F5B3',
-  },
-  'tomato juice': {
-    'color': '#D66413',
-  },
-  'pineapple juice': {
-    'color': '#F2ED4E',
-  },
-  'orange juice': {
-    'color': '#F5C451',
-  },
-  'tonic water': {
-    'color': '#F2F1CE',
-    'pattern': 'dots',
-    'patternColor': '#ffffff',
-  },
-  'water': {
-    'color': '#EDF7F6',
-    'pattern': 'stripes',
-    'patternColor': '#ffffff',
-  },
-  'milk': {
-    'color': '#ffffff',
-  },
-  'cream': {
-    'color': '#FAFAE3',
-    'pattern': 'stripes',
-    'patternColor': '#ffffff',
-  },
-  'sweet and sour mix': {
-    'color': '#EDF5C4',
-    'pattern': 'dots',
-    'patternColor': '#FFFC4D',
-  },
-  'hard cider': {
-    'color': '#DBC51F',
-    'pattern': 'dots',
-    'patternColor': '#F2E052',
-  },
-  'fireball': {
-    'color': '#EBAF0C',
-  },
-  "peach schnapps": {
-    'color': '#EBB65B',
-  },
-  "grenadine": {
-    'color': '#D67E7E',
-  },
-  "bitters": {
-    'color': '#C2BA99',
-    'pattern': 'dots',
-    'patternColor': '#FFFFFF',
-  }
-};
-
 const selectContents = (() => {
   let ret = '';
   Object.keys(drinkMap).sort().forEach((key) => {    
@@ -128,18 +6,31 @@ const selectContents = (() => {
   return ret;
 })();
 
-const glassHeight = 170;
+let glassHeight;
 const glassWidth = 150;
 
 let ctx;
 
-const convertToPercent = (drink) => {
+const convertToPercent = (glassType, drink) => {
   const total = drink.reduce((curVal, item) => {
     return curVal + item[1];
   }, 0);
-  drink.forEach((item) => {
-    item[1] = item[1] / total;
-  });
+  if (glassType === "martini") {
+    // fancy trig to get the correct %s of a triangle
+    let h1 = 0;
+    let h2;
+    let aG = glassHeight * glassHeight;
+    drink.forEach((item) => {
+      const aTrap = Math.floor((item[1] / total) * aG);
+      h2 = Math.sqrt(aTrap + (h1 * h1));
+      item[1] = (h2 - h1) / glassHeight;
+      h1 = h2;
+    });
+  } else {
+    drink.forEach((item) => {
+      item[1] = item[1] / total;
+    });
+  }
   return drink;
 };
 
@@ -196,27 +87,46 @@ const drawLayer = (layer, start) => {
   return start + height;
 };
 
-const drawDrink = (drink) => {
-  ctx.clearRect(0,0,1000,1000);
-  drink = convertToPercent(drink);
-  let pos = 10;
-  drink.reverse().forEach((item) => {
-    pos = drawLayer(item, pos);
-  });
-
-  // Draw the glass.
+const drawHighOrLowball = () => {
   ctx.fillStyle = '#FFFFFF';
   ctx.strokeStyle = '#000000';
   ctx.beginPath();
   ctx.moveTo(10.5,10.5);
-  ctx.lineTo(10.5,8.5);
-  ctx.lineTo(5.5,8.5);
-  ctx.lineTo(5.5,glassHeight + 12.5);
-  ctx.lineTo(glassWidth + 15.5,glassHeight + 12.5);
-  ctx.lineTo(glassWidth + 15.5,8.5);
-  ctx.lineTo(glassWidth + 10.5,8.5);
+  ctx.lineTo(10.5,3.5);
+  ctx.lineTo(5.5,3.5);
+  ctx.lineTo(5.5,glassHeight + 20.5);
+  ctx.lineTo(glassWidth + 15.5,glassHeight + 20.5);
+  ctx.lineTo(glassWidth + 15.5,3.5);
+  ctx.lineTo(glassWidth + 10.5,3.5);
   ctx.lineTo(glassWidth + 10.5,10.5);
   ctx.stroke();
+}
+
+const drawGlass = (glassType) => {
+  switch (glassType) {
+    case "highball":
+    case "lowball":
+      drawHighOrLowball();
+      break;
+    case "champagne":
+      break;
+    case "martini":
+      break;
+  }
+}
+
+const drawDrink = (json) => {
+  const glassType = json[0];
+  glassHeight = glassTypeHeightMap[glassType];
+  let drink = json[1];
+  ctx.clearRect(0,0,1000,1000);
+  drink = convertToPercent(glassType, drink);
+  let pos = 10;
+  drink.reverse().forEach((item) => {
+    pos = drawLayer(item, pos);
+  });
+  
+  drawGlass(glassType);
 };
 
 const formToJSON = () => {
@@ -237,7 +147,8 @@ const formToJSON = () => {
     drinkJSON += ']';
   }
   drinkJSON += ']';
-  return JSON.parse(drinkJSON);
+  let ret = '["' + document.querySelector('#glassTypes').value + '",' + drinkJSON+ ']'
+  return JSON.parse(ret);
 }
 
 const handleClick = () => {
---
+++ info.js
@@ -0,0 +1,128 @@
+const drinkMap = {
+  'bourbon': {
+    'color': '#A18950',
+    'pattern': 'stripes',
+    'patternColor': '#000000',
+  },
+  'whiskey': {
+    'color': '#CCB47C',
+    'pattern': 'stripes',
+    'patternColor': '#000000',
+  },
+  'brandy': {
+    'color': '#968432',
+    'pattern': 'stripes',
+    'patternColor': '#CCC08D',
+  },
+  'rum': {
+    'color': '#D6C163',
+  },
+  'creme de menthe': {
+    'color': '#DFF7D5',
+  },
+  'creme de cacao': {
+    'color': '#D6C47A',
+  },
+  'kahlua': {
+    'color': '#AD7F42',
+  },
+  'tequila': {
+    'color': '#E6FAE6',
+  },
+  'gin': {
+    'color': '#F7F5B5',
+  },
+  'vodka': {
+    'color': '#DEF9FF',
+  },
+  'cola': {
+    'color': '#B5A276',
+    'pattern': 'dots',
+    'patternColor': '#E0D8C5',
+  },
+  'sprite': {
+    'color': '#F7FAAF',
+    'pattern': 'dots',
+    'patternColor': '#ffffff',
+  },
+  'champagne': {
+    'color': '#F2E896',
+    'pattern': 'dots',
+    'patternColor': '#ffffff',
+  },
+  'iced tea': {
+    'color': '#E3BA3D',
+  },
+  'cherry juice': {
+    'color': '#9C4343',
+  },
+  'ginger ale': {
+    'color': '#DEC357',
+    'pattern': 'dots',
+    'patternColor': '#ffffff',
+  },
+  'lime juice': {
+    'color': '#9FF27E',
+  },
+  'lemon juice': {
+    'color': '#F5F5B3',
+  },
+  'tomato juice': {
+    'color': '#D66413',
+  },
+  'pineapple juice': {
+    'color': '#F2ED4E',
+  },
+  'orange juice': {
+    'color': '#F5C451',
+  },
+  'tonic water': {
+    'color': '#F2F1CE',
+    'pattern': 'dots',
+    'patternColor': '#ffffff',
+  },
+  'water': {
+    'color': '#EDF7F6',
+    'pattern': 'stripes',
+    'patternColor': '#ffffff',
+  },
+  'milk': {
+    'color': '#ffffff',
+  },
+  'cream': {
+    'color': '#FAFAE3',
+    'pattern': 'stripes',
+    'patternColor': '#ffffff',
+  },
+  'sweet and sour mix': {
+    'color': '#EDF5C4',
+    'pattern': 'dots',
+    'patternColor': '#FFFC4D',
+  },
+  'hard cider': {
+    'color': '#DBC51F',
+    'pattern': 'dots',
+    'patternColor': '#F2E052',
+  },
+  'fireball': {
+    'color': '#EBAF0C',
+  },
+  "peach schnapps": {
+    'color': '#EBB65B',
+  },
+  "grenadine": {
+    'color': '#D67E7E',
+  },
+  "bitters": {
+    'color': '#C2BA99',
+    'pattern': 'dots',
+    'patternColor': '#FFFFFF',
+  }
+};
+
+const glassTypeHeightMap = {
+  "highball": 250,
+  "lowball": 120,
+  "martini": 80,
+  "champagne": 130,
+};
